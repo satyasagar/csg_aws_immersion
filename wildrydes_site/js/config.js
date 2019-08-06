5c47b4bffea4564303d2b0770a0fbcd2d21e786b
vansat01 <satyasagar.vanteddu@gmail.com>
2019-08-06 15:58:45 +0000

    configure cognito

--- wildrydes_site/js/config.js
+++ wildrydes_site/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_rKK3FulPT', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '401sh37hq1mc244q6tk9fbc7j3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
